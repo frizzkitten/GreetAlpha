c686e72df50fa90575f8ee19d1b80bfb1a4788d9
Raizel Lieberman <crazyraizy@Raizels-MacBook-Pro.local>
2016-12-13 21:19:51 -0600

    Initial Commit

---
+++ greetAlpha2/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  greetAlpha2
+//
+//  Created by Raizel Lieberman on 12/13/16.
+//  Copyright © 2016 MyTech. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
